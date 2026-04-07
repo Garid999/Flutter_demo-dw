366972464992e3bb9696a2ae5a3e7d2435074a49
Garid999 <garidga999@gmail.com>
2026-04-07 13:38:18 +0800

    assignment 1

--- flutter_demo_2026-main/lib/services/service_locator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_demo/services/local_storage/local_storage.dart';
-import 'package:get_it/get_it.dart';
-
-final getIt = GetIt.instance;
-
-void setupServiceLocator() {
-  getIt.registerLazySingleton<LocalStorage>(() => LocalStorage());
-}
